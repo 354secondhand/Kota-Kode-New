b3feacf06e38a2ec973a0162771d1c27edf24d3f
Riyan Muhammad Fajar <109323283+354secondhand@users.noreply.github.com>
2022-09-01 16:50:14 +0700

    Kota Kode

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItem = [
+    {
+        title : 'Tentang',
+        url : '#',
+        cName :'nav-link'
+    },
+    {
+        title : 'Produk',
+        url : '#',
+        cName :'nav-link'
+    },
+    {
+        title : 'FAQ',
+        url : '#',
+        cName :'nav-link'
+    },
+    {
+        title : 'Events',
+        url : '#',
+        cName :'nav-link'
+    },
+    {
+        title : 'Partnership',
+        url : '#',
+        cName :'nav-link'
+    },
+    
+]
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Logo from '../Images/Logo.png'
+import './Navbar.css'
+import { MenuItem } from "./MenuItems";
+
+const Nav = () => {
+    return (
+            <header>
+            <div className="brand">
+                <a href="eaea.com">
+                <img src={Logo} alt='Kota Kode' className="logo"/>
+                </a>
+            </div>
+            <nav>
+                <ul className="nav-menu">
+                    {MenuItem.map((item, index) => {
+                        console.log ()
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>{item.title}</a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+            </header>
+    )
+}   
+
+
+export default Nav;
